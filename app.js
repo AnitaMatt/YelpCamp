49a8f699913a0094944735e627c0e1e155026ec4
AnitaMatt <arneethar16@yahoo.com>
2022-10-28 13:32:04 +0100

    feat: recently developed app

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require('ejs-mate');
+const methodOverride = require("method-override");
+const ExpressError = require("./utilities/ExpressError");
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+//IMPORTING ROUTERS
+const campgroundRoutes = require('./routes/campground');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
+
+// mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'secretforme',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash())
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.serializeUser());
+
+//flash setup ******************
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.edit = req.flash('edit');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+//Using routers
+app.get('/fakeUser',async (req, res)=>{
+    const user = new User({email: "ada@gmail.com", username:"coltp"})
+   const newuser = await User.register(user, "chick")
+   res.send(newuser)
+    
+})
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not found, try next time", 404))
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = "SOMETHING WENT WRONG"
+    res.status(statusCode).render("error", { err })
+})
+app.listen(3000, () => {
+    console.log("Serving on port 3000")
+})
